be86d83f182d36820e208abf89b6dc5fa09557d9
c00p75_ <georgecoopmsapenda@gmail.com>
2023-10-12 15:52:42 +0200

    create helper function to parse class names

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { cx } from '@/utils'
 import './globals.css'
 import { Inter, Manrope } from 'next/font/google'
 
@@ -12,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${manrope.variable} font-mr bg-light dark: bg-dark`}>{children}</body>
+      <body className={cx(inter.variable,
+        manrope.variable,
+        "font-mr bg-light dark:bg-dark"
+      )}>
+        {children}
+      </body>
     </html>
   )
 }
